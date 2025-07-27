69b2012e1812236b99e6609818661c495a321f72
Kalil-Camilo-009 <kalilcamilo205@gmail.com>
2025-07-27 13:58:05 -0400

    subir cambios

---
+++ Contactos moviles/Contactos moviles/Controllers/CuentaController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Contactos_moviles.Models;
+
+namespace Contactos_moviles.Controllers
+{
+    public class CuentaController : Controller
+    {
+        private readonly PlataformaDbContext _context;
+
+        public CuentaController(PlataformaDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string nombreUsuario, string clave)
+        {
+            var usuario = _context.Usuarios
+                .FirstOrDefault(u => u.NombreUsuario == nombreUsuario && u.Clave == clave);
+
+            if (usuario != null)
+            {
+                // Simulando sesión
+                TempData["usuario"] = usuario.NombreUsuario;
+                return RedirectToAction("Index", "Contactoes");
+            }
+            else
+            {
+                ViewBag.Error = "Credenciales incorrectas";
+                return View();
+            }
+        }
+
+        public IActionResult Logout()
+        {
+            TempData.Clear();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Contactos moviles/Contactos moviles/Models/PlataformaDbContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contactos_moviles.Models;
+
+public partial class PlataformaDbContext : DbContext
+{
+    public PlataformaDbContext()
+    {
+    }
+
+    public PlataformaDbContext(DbContextOptions<PlataformaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contacto> Contactos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-773V5TO;Database=personas_2;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contacto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Contacto__3214EC27EF3909AB");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Apellido).HasMaxLength(50);
+            entity.Property(e => e.Correo).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(50);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
